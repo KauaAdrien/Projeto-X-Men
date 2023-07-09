8d98c0dd91e89af1a1c5002eca6b5c91400a8a3f
“KauaAdrien” <kauaadrien76@gmail.com>
2023-07-08 22:20:14 -0400

    alt js2

--- src/js/index.js
+++ src/js/index.js
@@ -39,9 +39,8 @@ function alterarNomePersonagemSelecionado(personagem) {
 
 function alterarImagemPersonagemSelecionado(personagem) {
     const imagemPersonagemGrande = document.querySelector('.personagem-grande');
-    //     passo 2 - alterar a imagem do personagem grande
     const idPersonagem = personagem.attributes.id.value
-    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`
 }
 
 function removerSelecaoPersonagem() {
